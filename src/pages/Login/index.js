8096f32095fd3a4f9d036c321a9162e6914bd8a9
Evandro oliveira <vandoaparecido@hotmail.com>
2021-01-31 01:30:27 -0300

    Estrutura do projeto

---
+++ src/pages/Login/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {Container} from '../../styles';
+
+const Login = () => {
+  return (
+    <Container>
+      <Text>Login</Text>
+    </Container>
+  );
+};
+
+export default Login;
