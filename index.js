0cb22d066b90e2a125010c0469de1d754d77df2d
Evandro oliveira <vandoaparecido@hotmail.com>
2021-02-01 00:16:17 -0300

    Criando estrutura de Steps

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
  */
 
 import {AppRegistry} from 'react-native';
+
 import Login from './src/pages/Login';
+import Type from './src/pages/Steps/type';
+import Car from './src/pages/Steps/car';
+import Payment from './src/pages/Steps/payment';
+
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Login);
+AppRegistry.registerComponent(appName, () => Type);
---
+++ src/pages/Steps/type.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Type = () => {
+  return <Text>Type</Text>;
+};
+
+export default Type;
